8d6e78e5bb37c9068b45660abdb2d0dccff4bc00
Maula Izza Azizi <azizi.maula@gmail.com>
2022-11-01 00:23:35 +0700

    feat: add bounce animation

--- src/component/three/index.jsx
+++ src/component/three/index.jsx
@@ -3,6 +3,7 @@ import { PerspectiveCamera, OrbitControls, Environment } from "@react-three/drei
 import { useFrame } from "@react-three/fiber";
 import { useRef, useEffect } from "react";
 import * as THREE from "three";
+import gsap from "gsap";
 
 export class ThirdPersonCamera {
   constructor(params) {
@@ -12,8 +13,8 @@ export class ThirdPersonCamera {
 }
 
 export default function Three() {
+  // Code to move the camera around 
   const orbitControlsRef = useRef(null);
-
   useFrame((state, delta, xrFrame, yrFrame) => {
     if (!!orbitControlsRef.current) {
       const { x, y } = state.mouse;
@@ -23,13 +24,63 @@ export default function Three() {
     }
   });
 
-  requestAnimationFrame(() => {});
+  // requestAnimationFrame(() => {});
 
+  // useEffect(() => {
+  //   if (!!orbitControlsRef.current) {
+  //     console.log(orbitControlsRef.current);
+  //   }
+  // }, [orbitControlsRef.current]);
+
+  const ballRef = useRef(null)
   useEffect(() => {
-    if (!!orbitControlsRef.current) {
-      console.log(orbitControlsRef.current);
+    if(!!ballRef.current) {
+      console.log(ballRef.current)
+      // timeline
+      const timeline = gsap.timeline({paused : true})
+      
+      // [x] axis motion 
+      timeline.to(ballRef.current.position, {
+        x: 1,
+        duration: 2,
+        ease: "power2.out"
+      })
+
+      // // [y] axis motion
+      // timeline.to(ballRef.current.position, {
+      //   y: 0.5,
+      //   duration: 0.5,
+      //   ease: "power2.in"
+      // }, "<")
+      
+      // const coefficient = 0.8
+      // for (let i = 1; i <= 4; i++) {
+      //   // Going Up
+      //   timeline.to(ballRef.current.position, {
+      //     y: Math.pow(coefficient, i) * 1.5, 
+      //     duration: 0.2,
+      //     ease: "power2.out"
+      //   }, ">")
+        
+      //   // Going Down
+      //   timeline.to(ballRef.current.position, {
+      //     y: 0.5,
+      //     duration: 0.2,
+      //     ease: "power2.in"
+      //   }, ">")
+      // }
+
+      // [y] axis motion
+      timeline.to(ballRef.current.position, {
+        y: 0.5,
+        duration: 1,
+        ease: "bounce.out"
+      }, "<")
+      // timeline play
+      timeline.play()
     }
-  }, [orbitControlsRef.current]);
+    
+  }, [ballRef.current])
 
   return (
     <>
@@ -38,7 +89,7 @@ export default function Three() {
       <OrbitControls ref={orbitControlsRef} autoRotate={true} auto minPolarAngle={angleToRadians(60)} maxPolarAngle={angleToRadians(80)} />
 
       {/* Ball */}
-      <mesh position={[0, 0.5, 0]} castShadow>
+      <mesh position={[-2, 1.75, 0]} castShadow ref={ballRef}>
         <sphereGeometry args={[0.5, 32, 32]} />
         <meshStandardMaterial color="#ffffff" metalness={0.6} roughness={0.2} />
       </mesh>
