dccde5e197176fa7d1905dbf30df5e220d77208f
Maula Izza Azizi <72302421+storyofhis@users.noreply.github.com>
2022-10-31 15:48:38 +0700

    feat: add light, shadows, and environtment

--- src/component/three/index.jsx
+++ src/component/three/index.jsx
@@ -1,7 +1,8 @@
 import { angleToRadians } from "../../utils/angle";
-import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
+import { PerspectiveCamera, OrbitControls, Environment } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef, useEffect } from "react";
+import * as THREE from "three";
 
 export default function Three() {
   const orbitControlsRef = useRef(null);
@@ -9,8 +10,9 @@ export default function Three() {
   useFrame((state, delta, xrFrame, yrFrame) => {
     if (!!orbitControlsRef.current) {
       const { x, y } = state.mouse;
-      // orbitControlsRef.current.setAzimuthalAngle(-angleToRadians(x * 24));
-      // orbitControlsRef.current.update();
+      orbitControlsRef.current.setAzimuthalAngle(-x * angleToRadians(45));
+      orbitControlsRef.current.setAzimuthalAngle((y + 0.5) * angleToRadians(90 - 30));
+      orbitControlsRef.current.update();
     }
   });
 
@@ -26,22 +28,34 @@ export default function Three() {
     <>
       {/* Camera */}
       <PerspectiveCamera makeDefault position={[0, 1, 5]} />
-      <OrbitControls ref={orbitControlsRef} autoRotate={true} auto />
+      <OrbitControls ref={orbitControlsRef} autoRotate={true} auto minPolarAngle={angleToRadians(60)} maxPolarAngle={angleToRadians(80)} />
 
       {/* Ball */}
-      <mesh position={[0, 0.5, 0]}>
+      <mesh position={[0, 0.5, 0]} castShadow>
         <sphereGeometry args={[0.5, 32, 32]} />
-        <meshLambertMaterial color="#ffffff" />
+        <meshStandardMaterial color="#ffffff" metalness={0.6} roughness={0.2} />
       </mesh>
 
       {/* Floor */}
-      <mesh rotation={[-angleToRadians(90), 0, 0]}>
-        <planeGeometry args={[7, 7]} />
+      <mesh rotation={[-angleToRadians(90), 0, 0]} receiveShadow>
+        <planeGeometry args={[20, 20]} />
+        {/* <meshPhongMaterial color="#1ea3d8" /> */}
         <meshStandardMaterial color="#1ea3d8" />
       </mesh>
 
       {/* Ambient light */}
-      <ambientLight args={["#ffffff", 1]} />
+      <ambientLight args={["#ffffff", 0.25]} />
+
+      {/* SpotLight light */}
+      <spotLight args={["#ffffff", 1.5, 7, angleToRadians(45), 0.4]} position={[-3, 1, 0]} castShadow />
+
+      {/* Environment */}
+      <Environment background>
+        <mesh>
+          <sphereGeometry args={[50, 100, 100]} />
+          <meshBasicMaterial side={THREE.BackSide} color="#2266cc" />
+        </mesh>
+      </Environment>
     </>
   );
 }
